2d65c7bd1a0566825eedf76ad84342f38f04870f
mendapara-snowflake <119445134+mendapara-snowflake@users.noreply.github.com>
2022-12-02 18:04:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
 # streamlit.header("Add fruit of your choice!")
 
 def insert_row_snowflake(new_fruit):
-  with my_cnx_cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlist')")
     return "Thanks for adding " + new_fruit
 
